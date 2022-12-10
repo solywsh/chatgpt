bee5ee4bf80f0114dd483d12e46cd1e738365355
solywsh <dont@panic.ltd>
2022-12-11 00:29:16 +0800

    fix(chat): fix chat answer format

--- context.go
+++ context.go
@@ -102,7 +102,7 @@ func (c *ChatGPT) ChatWithContext(question string) (answer string, err error) {
 	if err != nil {
 		return "", err
 	}
-	formatAnswer(resp.Choices[0].Text)
+	resp.Choices[0].Text = formatAnswer(resp.Choices[0].Text)
 	c.ChatContext.old = append(c.ChatContext.old, conversation{
 		role:   c.ChatContext.humanRole,
 		prompt: question,
